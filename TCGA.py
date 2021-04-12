fc1506411824ef0da47c59cbe5f8cb7dca380811
Marcos Parra <mfrparra@gmail.com>
2021-04-11 23:49:07 -0300

    TCGA Versão final da Classe TCGA.

--- TCGA.py
+++ TCGA.py
@@ -10,7 +10,7 @@ import csv
 
 #Retorna os dados clínicos do Paciente pelo ID
 def retornaPaciente(UUID):
-    with open("Data_TCGA_BRCA/CasosClinicos/clinical.tsv") as tsv:
+    with open("clinical.tsv") as tsv:
         linhasArquivo = csv.reader(tsv, dialect="excel-tab")
         for i, linha in enumerate(csv.reader(tsv, dialect="excel-tab")):
             if (i % 2 != 0):
@@ -69,6 +69,11 @@ def retornaPaciente(UUID):
                     #further divided to provide more detail, such as T3a and T3b.
                     ajccPatT = linha[27]
 
+                    #Primary Diagnosis
+                    #Infiltrating duct carcinoma, NOS
+                    #Lobular carcinoma, NOS
+                    #Infiltrating duct and lobular carcinoma
+                    #Infiltrating duct mixed with other types of carcinoma
                     primaryDiagnosis = linha[107]
 
 
@@ -77,26 +82,166 @@ def retornaPaciente(UUID):
 
                     tumorStage = linha[127]
 
+                    #Binário: sim / não
                     tratamento = linha[151]
+                    
+                    #Pharmaceutical / Radiation
                     tipoTratamento = linha[153]
 
+                    #Correção dos dados de raça
                     if(raca == "black or african american"):
                         raca = "black"
                     elif(raca == "not reported"):
-                        raca = "white"
+                        raca = "black"
                     print(pacienteID, "|", idade, "|", etnia, "|", genero, "|", raca, "|", ajccPatT, "|",ajccPatM, "|",ajccPatN, "|",ajccPatStage, "|",primaryDiagnosis
                             , "|", tumorStage, "|", tratamento, "|", tipoTratamento)
+                    
+
+                    #Correção dos dados de Stage
+                    if(ajccPatStage == "Stage IA"):
+                        ajccPatStage = "Stage I"
+                    elif(ajccPatStage == "Stage IB"):
+                        ajccPatStage = "Stage I"
+                    elif(ajccPatStage == "Stage IC"):
+                        ajccPatStage = "Stage I"
+                    elif(ajccPatStage == "Stage IIA"):
+                        ajccPatStage = "Stage II"
+                    elif(ajccPatStage == "Stage IIB"):
+                        ajccPatStage = "Stage II"
+                    elif(ajccPatStage == "Stage IIC"):
+                        ajccPatStage = "Stage II"
+                    elif(ajccPatStage == "Stage IIIA"):
+                        ajccPatStage = "Stage III"
+                    elif(ajccPatStage == "Stage IIIB"):
+                        ajccPatStage = "Stage III"
+                    elif(ajccPatStage == "Stage IIIC"):
+                        ajccPatStage = "Stage III"
+
+
+                    #Correção dados Primary tumor (T)
+                    if(ajccPatT == "T1a"):
+                        ajccPatT = "T1"
+                    elif(ajccPatT == "T1b"):
+                        ajccPatT = "T1"
+                    elif(ajccPatT == "T1c"):
+                        ajccPatT = "T1"
+
+                    #Correção Distant metastasis (M)
+                    if(ajccPatM == "cM0 (i+)"):
+                        ajccPatM = "M0"
+
+                    #Correção Regional lymph nodes (N)
+                    if(ajccPatN == "N0 (i-)"):
+                        ajccPatN = "N0"
+                    elif(ajccPatN == "N0 (i+)"):
+                        ajccPatN = "N0"
+                    elif(ajccPatN == "N1a"):
+                        ajccPatN = "N1"
+                    elif(ajccPatN == "N1b"):
+                        ajccPatN = "N1"
+                    elif(ajccPatN == "N1mi"):
+                        ajccPatN = "N1"
+                    elif(ajccPatN == "N2a"):
+                        ajccPatN = "N2"
+                    elif(ajccPatN == "N3a"):
+                        ajccPatN = "N3"
+                    
+
 
                     #print(linha)
 
-def retornaPacienteStage(UUID):
-    with open("Data_TCGA_BRCA/CasosClinicos/clinical.tsv") as tsv:
+def retornaPacienteDadoClinico(UUID, dadoClinico):
+    with open("clinical.tsv") as tsv:
         linhasArquivo = csv.reader(tsv, dialect="excel-tab")
         for i, linha in enumerate(csv.reader(tsv, dialect="excel-tab")):
             if (i % 2 != 0):
                 if (linha[1]==UUID):
-                    ajccPatStage = linha[26]
-    return(ajccPatStage)
+                    if (dadoClinico == "Distant metastasis"):
+                        #Distant metastasis (M)
+                        #MX: Metastasis cannot be measured.
+                        #M0: Cancer has not spread to other parts of the body.
+                        #M1: Cancer has spread to other parts of the body.
+                        dadoPaciente = linha[24]
+
+                        #Correção Distant metastasis (M)
+                        if(dadoPaciente == "cM0 (i+)"):
+                            dadoPaciente = "M0"
+                    
+                    elif (dadoClinico == "Regional lymph nodes"):
+                        #Regional lymph nodes (N)
+                        #NX: Cancer in nearby lymph nodes cannot be measured.
+                        #N0: There is no cancer in nearby lymph nodes.
+                        #N1, N2, N3: Refers to the number and location of lymph nodes 
+                        #that contain cancer. The higher the number after the N, the 
+                        #more lymph nodes that contain cancer.
+                        dadoPaciente = linha[25]
+
+                        #Correção Regional lymph nodes (N)
+                        if(dadoPaciente == "N0 (i-)"):
+                            dadoPaciente = "N0"
+                        elif(dadoPaciente == "N0 (i+)"):
+                            dadoPaciente = "N0"
+                        elif(dadoPaciente == "N1a"):
+                            dadoPaciente = "N1"
+                        elif(dadoPaciente == "N1b"):
+                            dadoPaciente = "N1"
+                        elif(dadoPaciente == "N1mi"):
+                            dadoPaciente = "N1"
+                        elif(dadoPaciente == "N2a"):
+                            dadoPaciente = "N2"
+                        elif(dadoPaciente == "N3a"):
+                            dadoPaciente = "N3"
+
+                    elif (dadoClinico == "Primary tumor"):
+                        #Primary tumor (T)
+                        #TX: Main tumor cannot be measured.
+                        #T0: Main tumor cannot be found.
+                        #T1, T2, T3, T4: Refers to the size and/or extent of the main
+                        #tumor. The higher the number after the T, the larger the tumor
+                        #or the more it has grown into nearby tissues. T's may be
+                        #further divided to provide more detail, such as T3a and T3b.
+                        dadoPaciente = linha[27]
+
+                        #Correção dados Primary tumor (T)
+                        if(dadoPaciente == "T1a"):
+                            dadoPaciente = "T1"
+                        elif(dadoPaciente == "T1b"):
+                            dadoPaciente = "T1"
+                        elif(dadoPaciente == "T1c"):
+                            dadoPaciente = "T1"
+
+                    
+                    elif (dadoClinico == "Stage"):
+                        dadoPaciente = linha[26]
+
+                        #Correção dos dados de Stage
+                        if(dadoPaciente == "Stage IA"):
+                            dadoPaciente = "Stage I"
+                        elif(dadoPaciente == "Stage IB"):
+                            dadoPaciente = "Stage I"
+                        elif(dadoPaciente == "Stage IC"):
+                            dadoPaciente = "Stage I"
+                        elif(dadoPaciente == "Stage IIA"):
+                            dadoPaciente = "Stage II"
+                        elif(dadoPaciente == "Stage IIB"):
+                            dadoPaciente = "Stage II"
+                        elif(dadoPaciente == "Stage IIC"):
+                            dadoPaciente = "Stage II"
+                        elif(dadoPaciente == "Stage IIIA"):
+                            dadoPaciente = "Stage III"
+                        elif(dadoPaciente == "Stage IIIB"):
+                            dadoPaciente = "Stage III"
+                        elif(dadoPaciente == "Stage IIIC"):
+                            dadoPaciente = "Stage III"
+                    
+                    elif (dadoClinico == "Tratamento"):
+                        #Binário: sim / não
+                        dadoPaciente = linha[151]
+
+                    elif (dadoClinico == "tipoTratamento"):
+                        #Pharmaceutical / Radiation
+                        dadoPaciente = linha[153]
+    return(dadoPaciente)
 
 
 
